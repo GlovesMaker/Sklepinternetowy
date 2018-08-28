1efd7a9d206fdf14cbe7c6d5833da33b3bd86988
GlovesMaker <buchar123@gmail.com>
2018-08-28 23:16:51 +0200

    'Projekt_saaaa'

--- cart/cart.py
+++ cart/cart.py
@@ -17,8 +17,7 @@ class Cart(object):
     def add(self, product, quantity=1, update_quantity=False):
 
         product_id = str(product.id)
-        if product.stock <= quantity:
-            
+        if product.stock >= quantity:
             if not product_id in self.cart:
                 self.cart[product_id] = self.cart.get(product_id, {'quantity': quantity, 'price': str(product.price)})
 
